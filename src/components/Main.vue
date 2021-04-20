0f574b2421c47e4d32266d79f62beeb18928c739
Yassine <yassineattari64@gmail.com>
2021-04-20 15:00:03 +0000

    feat: wip 40%

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,26 +1,41 @@
 <template>
   <b-container class="bv-example-row">
-    <b-form-select v-model="selectedCountry" :options="options"></b-form-select>
+    <b-form-select
+      v-model="selectedCountryByCode"
+      @change="HandleSelectedCountry"
+      :options="options"
+    ></b-form-select>
     <div class="mt-3">
-      Selected: <strong>{{ selectedCountry }}</strong>
+      Selected: <strong>{{ selectedCountryFullName }}</strong>
     </div>
   </b-container>
 </template>
 
 <script>
 import * as countries from "../helpers/countries";
+
 export default {
   data() {
     return {
-      selectedCountry: "MA",
+      selectedCountryByCode: "MA",
+      selectedCountryFullName: "",
       options: countries.countries,
     };
   },
   created() {
-    let countryName = countries.countries.find(
-      (country) => country.value == this.selectedCountry
-    ).text;
-    console.log(countryName);
+    console.log(this.GetCountryFullNameByCode);
+  },
+  methods: {
+    HandleSelectedCountry() {
+      this.selectedCountryFullName = this.GetCountryFullNameByCode;
+    },
+  },
+  computed: {
+    GetCountryFullNameByCode() {
+      return this.options.find(
+        (country) => country.value == this.selectedCountryByCode
+      ).text;
+    },
   },
 };
 </script>
