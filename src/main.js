8ee7dbbc0bfe118250f9714045b6729bb83c8a75
MouadZIANI <mouad.ziani1997@gmail.com>
2021-04-25 12:06:28 +0000

    feat: make countries/cities selects searchable

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-search-select/dist/VueSearchSelect.css'
 
 //simple global css
 import '../src/assets/style.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
+
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
